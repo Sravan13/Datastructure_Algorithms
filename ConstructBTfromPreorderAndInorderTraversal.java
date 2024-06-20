d1bed6195f85237f29d42b74b170b3f83d8652f4
Sravan13 <chithari.sravan@gmail.com>
2024-06-20 14:22:35 +0530

    Update ConstructBTfromPreorderAndInorderTraversal.java

--- ConstructBTfromPreorderAndInorderTraversal.java
+++ ConstructBTfromPreorderAndInorderTraversal.java
@@ -1,7 +1,8 @@
 
 /*
  * 
- * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+ * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the 
+ inorder traversal of the same tree, construct and return the binary tree.
 
  
 
