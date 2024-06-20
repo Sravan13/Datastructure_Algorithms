7c59b5f80e3e15447e9fe3591172ce010bba0040
Sravan13 <chithari.sravan@gmail.com>
2024-06-20 14:01:39 +0530

    Update CheckIfItIsAStraightLine.java

--- CheckIfItIsAStraightLine.java
+++ CheckIfItIsAStraightLine.java
@@ -1,5 +1,6 @@
 /*
- * You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+ * You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents 
+ the coordinate of a point. Check if these points make a straight line in the XY plane.
 
  
 
