2192e8d86f370d8da4e9b811c115940cc7befee3
Sravan13 <chithari.sravan@gmail.com>
2024-10-01 15:27:15 +0530

    Update ValidSequenceFromRootToLeavesPathInBT.java

--- ValidSequenceFromRootToLeavesPathInBT.java
+++ ValidSequenceFromRootToLeavesPathInBT.java
@@ -2,9 +2,11 @@
 
 /*
  * Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
-Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
+Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a 
+given string is a valid sequence in such binary tree. 
 
-We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+We get the given string from the concatenation of an array of integers arr and the concatenation of all values 
+of the nodes along a path results in a sequence in the given binary tree.
 
  
 
